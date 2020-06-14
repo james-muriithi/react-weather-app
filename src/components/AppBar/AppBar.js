aa17e4280893671791366b8cb0774370275ecb6a
james-muriithi <muriithijames556@gmail.com>
2020-06-15 01:55:17 +0300

    added appbar

---
+++ src/components/AppBar/AppBar.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import InfoIcon from '@material-ui/icons/InfoOutlined';
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1
+    },
+    title: {
+        flexGrow: 1,
+        display: 'block',
+        textAlign: 'left'
+    }
+})
+
+class MyAppBar extends Component {
+    render() {
+        const { classes } = this.props;
+        return (
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Typography className={classes.title} variant="h6" noWrap>
+                            Weather App
+                        </Typography>
+
+                        <IconButton aria-label="display more actions" edge="end" color="inherit">
+                            <InfoIcon />
+                        </IconButton>
+
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
+    }
+}
+
+export default withStyles(styles)(MyAppBar);
