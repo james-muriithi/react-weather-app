d7adb72c60419ffee3f2dee0b2c9e6deb4472a49
james-muriithi <muriithijames556@gmail.com>
2020-06-15 16:41:23 +0300

    converted toa gatsby site

---
+++ gatsby-config.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+
+module.exports = {
+    siteMetadata: {
+        title: `React Wather App`,
+        description: `PWA Weather Application made with React/Material Ui/Gatsby.This is a personal project built in my spare time for learning purposes.`,
+        author: `James Muriithi <muriithijames556@gmail.com>`,
+        siteUrl: `https://james-muriithi.github.io/react-weather-app/`,
+        siteLanguage: `en`,
+        ogLanguage: `en`,
+        headline: `React Wather App`,
+        twitter: `@jam_es_muriithi`,
+        facebook: `james-muriithi`,
+        banner: `/me-icon.jpg`
+    },
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/assets/images/`,
+            },
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `React Weather App`,
+                short_name: `Weather App`,
+                start_url: `/`,
+                background_color: `#1976d2`,
+                theme_color: `#0d4072`,
+                display: `standalone`,
+                icon: `src/assets/images/logo192.png`, // This path is relative to the root of the site.
+            },
+        },
+        `gatsby-plugin-advanced-sitemap`,
+        {
+            resolve: `gatsby-plugin-offline`,
+            options: {
+                precachePages: [ `/` ],
+            },
+        }
+    ],
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react"
+import Main from "../App"
 import { injectGlobal } from 'styled-components';
 import 'weathericons/css/weather-icons.min.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 injectGlobal`
   @import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500');
-
   * {
     box-sizing: border-box;
   }
-
   body {
     margin: 0;
     padding: 0;
     font-family: 'Roboto', sans-serif;
     background-color: #fff;
-
     @media (max-width: 1000px) {
       background-color: white;
     }
   }
 `;
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const IndexPage = () => {
+    return (
+        <>
+            <Main />        
+        </>
+    )
+}
+
+export default IndexPage
